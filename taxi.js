573e8455352a451ff096f6170d78b533f308dcc0
andiswa-sinxo <andiswasinxo@gmail.com>
2022-05-23 16:49:14 +0200

    taxi trip

---
+++ taxi.js
@@ -0,0 +1,105 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('route', () => ({
+        mainRank: 'Cape Town',
+        init() {
+            console.log(this.rank)
+        },
+        rank: [
+            {
+                destination: "Gugulethu",
+                passageAction: 0,
+                taxiTrips: 0,
+                taxiFare: 20.00,
+                totalFare: 0.00,
+                taxiAvail: 4,
+                limit: 10,
+                
+                    add(){
+                        this.passageAction++;
+                    },
+                    subtract(){
+                        this.passageAction--
+                    },
+                    leave(){
+                        this.taxiTrips++;
+                        this.taxiAvail -=1;
+                        this.passageAction -= this.limit
+                    },
+                    total(){
+                        this.taxiTrips + this.taxiFare
+                    }
+
+                    
+            },
+            {
+                destination: "Nyanga",
+                passageAction: 0,
+                taxiTrips: 0,
+                taxiFare: 21.00,
+                totalFare: 0.00,
+                taxiAvail: 3,
+                limit: 10,
+
+                add(){
+                    this.passageAction++;
+                },
+                subtract(){
+                    this.passageAction--
+                },
+                leave(){
+                    this.taxiTrips++;
+                    this.taxiAvail -=1;
+                    this.passageAction -= this.limit
+                }
+            },
+            {
+                destination: "Khayelitsha",
+                passageAction: 0,
+                taxiTrips: 0,
+                taxiFare: 35.00,
+                totalFare: 0.00,
+                taxiAvail: 7,
+                limit: 10,
+
+                add(){
+                    this.passageAction++;
+                },
+                subtract(){
+                    this.passageAction--
+                },
+                leave(){
+                    this.taxiTrips++;
+                    this.taxiAvail -=1;
+                    this.passageAction -= this.limit
+
+                }
+            },
+            {
+                destination: "Thornton",
+                passageAction: 0,
+                taxiTrips: 0,
+                taxiFare: 18.00,
+                totalFare: 0.00,
+                taxiAvail: 2,
+                limit: 10,
+
+                add(){
+                    this.passageAction++;
+                },
+                subtract(){
+                    this.passageAction--
+                },
+                leave(){
+                    this.taxiTrips++;
+                    this.taxiAvail -=1;
+                    this.passageAction -= this.limit
+                }
+            }
+            
+    
+        ]
+       
+    }))
+})
+
+
